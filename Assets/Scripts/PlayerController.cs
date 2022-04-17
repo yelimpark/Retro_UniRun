6c65bbb9fd2e3cee3fcfc6becf5918bccec58110
yelimpark <pyl9983@gmail.com>
2022-04-17 13:17:30 +0900

    [Update] chapter11

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+// PlayerController는 플레이어 캐릭터로서 Player 게임 오브젝트를 제어한다.
+public class PlayerController : MonoBehaviour {
+   public AudioClip deathClip; // 사망시 재생할 오디오 클립
+   public float jumpForce = 700f; // 점프 힘
+
+   private int jumpCount = 0; // 누적 점프 횟수
+   private bool isGrounded = false; // 바닥에 닿았는지 나타냄
+   private bool isDead = false; // 사망 상태
+
+   private Rigidbody2D playerRigidbody; // 사용할 리지드바디 컴포넌트
+   private Animator animator; // 사용할 애니메이터 컴포넌트
+   private AudioSource playerAudio; // 사용할 오디오 소스 컴포넌트
+
+   private void Start() {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+   }
+
+   private void Update() {
+        if (isDead)
+        {
+            return;
+        }
+
+        if (Input.GetMouseButtonDown(0) && jumpCount < 2)
+        {
+            jumpCount++;
+            playerRigidbody.velocity = Vector2.zero;
+            playerRigidbody.AddForce(new Vector2(0, jumpForce));
+            playerAudio.Play();
+        }
+        else if (Input.GetMouseButtonUp(0) && playerRigidbody.velocity.y > 0)
+        {
+            // ~~ 속도의 y 값이 양수, 위로 상승중이라면 
+            playerRigidbody.velocity = playerRigidbody.velocity * 0.5f;
+        }
+
+        animator.SetBool("Grounded", isGrounded);
+   }
+
+   private void Die() {
+        animator.SetTrigger("Die");
+        playerAudio.clip = deathClip;
+        playerAudio.Play();
+
+        // 속도를 제로(0,0)로 변경
+        playerRigidbody.velocity = Vector2.zero;
+        isDead = true;
+   }
+
+   private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Dead" && !isDead)
+        {
+            Die();
+        }
+   }
+
+   private void OnCollisionEnter2D(Collision2D collision) {
+        // 어떤 콜라이더와 닿았으며, 충돌 표면이 위쪽을 보고 있으면
+        if (collision.contacts[0].normal.y > 0.7f)
+        {
+            isGrounded = true;
+            jumpCount = 0;
+        }
+   }
+
+   private void OnCollisionExit2D(Collision2D collision) {
+        isGrounded = false;
+   }
+}
